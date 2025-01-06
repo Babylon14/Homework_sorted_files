24baef9fb4cce42fd08aab278790f697c6ca8a33
Andrey Zablotskiy <zablotskyandrew@yandex.ru>
2025-01-06 14:17:55 +0400

    Added a loopthat reads all files

--- union_files.py
+++ union_files.py
@@ -2,25 +2,18 @@ import os
 
 file_directory = os.path.dirname(os.path.abspath(__file__))
 
-file_path_1 = os.path.join(file_directory, "1.txt")
-file_path_2 = os.path.join(file_directory, "2.txt")
-file_path_3 = os.path.join(file_directory, "3.txt")
-
-
 result_file_info = []
-
 def read_file(file_path):
     with open(file_path, "r", encoding="utf-8") as file:
         lines = [line.strip() for line in file.readlines()]
         return lines, len(lines)
     
-text_file_1, length_file_1 = read_file(file_path_1)
-text_file_2, length_file_2 = read_file(file_path_2)
-text_file_3, length_file_3 = read_file(file_path_3)
+for filename in os.listdir(file_directory):
+    file_path = os.path.join(file_directory, filename)
 
-result_file_info.append((os.path.basename(file_path_1), length_file_1, *text_file_1))
-result_file_info.append((os.path.basename(file_path_2), length_file_2, *text_file_2))
-result_file_info.append((os.path.basename(file_path_3), length_file_3, *text_file_3))
+    if os.path.isfile(file_path) and filename.endswith(".txt"):
+        text_file, length_file = read_file(file_path)
+        result_file_info.append((filename, length_file, *text_file))
 
 result_file_info.sort(key=lambda x: x[1])
 
